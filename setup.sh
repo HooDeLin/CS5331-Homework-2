fd3668e3e28c73d07cc80e59a4c550e26c9b89a4
HooDeLin <hoodelin0619@gmail.com>
2018-03-18 10:16:16 +0800

    Adding chromium dependencies

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ set -e
 
 sudo apt-get update -y
 sudo apt-get install python-pip -y
+sudo apt-get install chromium-browser -y
 sudo pip install requests
 sudo pip install selenium
 sudo apt-get install firefox -y
